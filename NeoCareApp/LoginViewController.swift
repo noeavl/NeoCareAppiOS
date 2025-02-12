62b75bb0d7a3ede3d4c81b1888b7a94c250abd0a
noahx0001 <lopeznoe191119@gmail.com>
2025-02-12 13:07:56 -0600

    First Commit

---
+++ NeoCareApp/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  NeoCareApp
+//
+//  Created by Noe  on 12/02/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- NeoCareApp/ViewController.swift
+++ NeoCareApp/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txfEmail: UITextField!
+    @IBOutlet weak var txfPassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        txfEmail.layer.cornerRadius = 20
     }
-
 
 }
 
