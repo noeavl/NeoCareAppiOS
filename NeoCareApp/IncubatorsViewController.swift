fd1e7c330dc66ef1aec6c5cb0df6dd1b00490cba
JuanGmz <juanalfredohd@gmail.com>
2025-02-17 00:42:43 -0600

    Creación de IncubadorasViewController

---
+++ NeoCareApp/IncubatorsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  IncubatorsViewController.swift
+//  NeoCareApp
+//
+//  Created by Juan Gómez on 17/02/25.
+//
+
+import UIKit
+
+class IncubatorsViewController: UIViewController {
+    @IBOutlet weak var viewButton: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
