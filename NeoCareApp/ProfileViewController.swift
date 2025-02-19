ea701b97f7a3ca730f13bcf126666ef98a9214d4
zanwonzzzz <etnicastro9@gmail.com>
2025-02-19 12:43:16 -0600

    Profile

---
+++ NeoCareApp/ProfileViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileViewController.swift
+//  NeoCareApp
+//
+//  Created by mac on 19/02/25.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        profileImageView.layer.cornerRadius = profileImageView.frame.size.width / 2
+        profileImageView.clipsToBounds = true
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
