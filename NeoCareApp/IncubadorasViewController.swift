5a8ce0e9b2d72354dec0aa136db3f1b3e3bffa4f
JuanGmz <juanalfredohd@gmail.com>
2025-02-21 21:56:12 -0600

    incubator view is already rounded

---
+++ NeoCareApp/IncubadorasViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IncubadorasViewController.swift
+//  NeoCareApp
+//
+//  Created by Juan Gómez on 21/02/25.
+//
+
+import UIKit
+
+class IncubadorasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
