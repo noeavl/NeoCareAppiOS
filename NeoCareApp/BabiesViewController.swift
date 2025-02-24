69d2f43051678633c3e558cbe24df2f860d08f34
jtalaveraa <jiramtalavera@hotmail.com>
2025-02-24 02:45:11 -0600

    feat(babiesTableView): Funcionalidad en Table View de Babies

--- NeoCareApp/BabiesViewController.swift
+++ NeoCareApp/BabiesViewController.swift
@@ -7,23 +7,59 @@
 
 import UIKit
 
-class BabiesViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class BabiesViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return babies.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BabyCell", for: indexPath)as!
+            BabyTableViewCell
+        let baby = babies[indexPath.row]
+        cell.lblName.text = baby.name
+        cell.lblBirthDate.text = dateFormatter.string(from: baby.birthdate)
+        cell.lblIncubator.text =
+           String(baby.incubator)
+        return cell
     }
-    */
-
+    
+    
+    
+    @IBOutlet weak var tableViewBabies: UITableView!
+    struct baby{
+        let id:Int
+        let name:String
+        let birthdate:Date
+        let incubator:Int
+        
+    }
+    
+    private let dateFormatter: DateFormatter = {
+           let formatter = DateFormatter()
+           formatter.dateFormat = "yyyy-MM-dd"
+           return formatter
+       }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableViewBabies.dataSource = self
+        
+    }
+    
+    
+    
+    var babies: [baby] = [
+        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
+        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
+        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
+        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
+        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
+        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
+        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
+        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1)
+    ]
+    
+    
+    
 }
+
