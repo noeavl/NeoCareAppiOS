1097e272a0c2956c2895ebae505f75820716964b
JuanGmz <juanalfredohd@gmail.com>
2025-02-27 00:12:52 -0600

    table view lista, no los borres tala, haz paro porfa

---
+++ NeoCareApp/IncubatorsTableTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  IncubatorsTableTableViewCell.swift
+//  NeoCareApp
+//
+//  Created by Juan Gómez on 26/02/25.
+//
+
+import UIKit
+
+class IncubatorsTableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
