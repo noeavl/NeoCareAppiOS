f46797f3acc3be2c06770dff44e678cfe7bfbff6
noahx0001 <lopeznoe191119@gmail.com>
2025-03-26 20:32:23 -0600

    Logout Listo con consumo de ruta de logout de la API para borrar el token.

--- NeoCareApp/ProfileViewController.swift
+++ NeoCareApp/ProfileViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var imvProfile: UIImageView!
+    let loginEndpoint = "http://34.215.209.108/api/v1/sessions/logout"
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,8 +40,37 @@ class ProfileViewController: UIViewController {
             
             // Acción para confirmar el logout
             let confirmAction = UIAlertAction(title: "Log Out", style: .destructive) { _ in
-                AuthManager.shared.logout()
-                self.performSegue(withIdentifier: "sgProfileLogin", sender: nil)
+            
+                // Consumo de la ruta de logout para borrar el token de la API
+                do{
+                    let request = try self.createURLRequest()
+                    URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+                        if let error = error {
+                            self?.showToast(message: "\(error.localizedDescription)", title: "Network Error")
+                        }
+                        
+                        let statusCode: Int
+                        if let httpResponse = response as? HTTPURLResponse {
+                            statusCode = httpResponse.statusCode
+                        } else {
+                            statusCode = 0
+                            self?.showToast(message: "Invalid Response.")
+                        }
+                        DispatchQueue.main.async {
+                            switch statusCode {
+                            case 200...299:
+                                self?.showToast(message: "Log Out Successfully", title: "Success")
+                            default:
+                                self?.showToast(message: "Please log in again.", title: "Error")
+                            }
+                        }
+                    }.resume()
+                }catch{
+                    self.showToast(message: "Error creating the request.")
+                }
+                
+            // Borrar el token del singleton y redireccionar.
+            AuthManager.shared.logout()
             }
             
             // Acción para cancelar
@@ -50,6 +80,44 @@ class ProfileViewController: UIViewController {
             alert.addAction(cancelAction)
             
             present(alert, animated: true, completion: nil)
+    }
+    
+    // Alerta/Toast se quita automaticamente.
+    private func showToast(message: String,
+                          duration: TimeInterval = 1.5,
+                          title: String? = nil,
+                          alertStyle: UIAlertController.Style = .alert) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(
+                title: title,
+                message: message,
+                preferredStyle: alertStyle
+            )
+            
+            self.present(alert, animated: true)
+            
+            // Temporizador para cerrar automáticamente
+            Timer.scheduledTimer(withTimeInterval: duration, repeats: false) { _ in
+                // Cerrar alerta y redireccion al Login.
+                alert.dismiss(animated: true){
+                    self.performSegue(withIdentifier: "sgProfileLogin", sender: nil)
+                }
+            }
+        }
+    }
+    
+    private func createURLRequest() throws -> URLRequest {
+        guard let url = URL(string: loginEndpoint) else {
+            throw NetworkError.invalidURL
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        if let token = AuthManager.shared.loadToken() {
+            request.addValue(String("Bearer \(token)"), forHTTPHeaderField: "Authorization")
+        }
+        return request
     }
     
 }
