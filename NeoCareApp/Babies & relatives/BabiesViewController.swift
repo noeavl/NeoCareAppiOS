dfe063458a96b2143a2f47861ba0f288929f17a6
jtalaveraa <jiramtalavera@hotmail.com>
2025-04-01 05:25:03 -0600

    index babies & relatives

--- NeoCareApp/Babies & relatives/BabiesViewController.swift
+++ NeoCareApp/Babies & relatives/BabiesViewController.swift
@@ -7,63 +7,213 @@
 
 import UIKit
 
-class BabiesViewController: UIViewController, UITableViewDataSource {
+class BabiesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var viewAdd: UIView!
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return babies.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "BabyCell", for: indexPath)as!
-            BabyTableViewCell
-        let baby = babies[indexPath.row]
-        cell.lblName.text = baby.name
-        cell.lblBirthDate.text = dateFormatter.string(from: baby.birthdate)
-        cell.lblIncubator.text =
-           String(baby.incubator)
-        return cell
-    }
-    
-    
-    
     @IBOutlet weak var tableViewBabies: UITableView!
-    struct baby{
-        let id:Int
-        let name:String
-        let birthdate:Date
-        let incubator:Int
-        
-    }
     
     private let dateFormatter: DateFormatter = {
-           let formatter = DateFormatter()
-           formatter.dateFormat = "yyyy-MM-dd"
-           return formatter
-       }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tableViewBabies.dataSource = self
-        viewAdd.roundCorners([.allCorners], 40.0)
+               let formatter = DateFormatter()
+               formatter.dateFormat = "yyyy-MM-dd"
+               return formatter
+           }()
         
-    }
+        var babies: [Baby] = []
+        let babiesEndPoint = "http://34.215.209.108/api/v1/babiesNoPaginate"
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setupActivityIndicator()
+            tableViewBabies.dataSource = self
+            tableViewBabies.delegate = self
+            viewAdd.roundCorners([.allCorners], 100.0)
+            
+            getBabies()
+        }
+        
+        private func setupActivityIndicator() {
+                view.addSubview(activityIndicator)
+                
+                NSLayoutConstraint.activate([
+                    activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                    activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+                ])
+            }
+        
+        private func createURLRequest() throws -> URLRequest {
+            guard let url = URL(string: babiesEndPoint ) else {
+                throw NetworkError.invalidURL
+            }
+            
+            let token = AuthManager.shared.loadToken() ?? ""
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("Bearer "+token, forHTTPHeaderField: "Authorization")
+            return request
+        }
+        
+        private func getBabies(){
+            activityIndicator.startAnimating()
+            do{
+                let request = try createURLRequest()
+                
+                URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+                    
+                    DispatchQueue.main.async {
+                                        self?.activityIndicator.stopAnimating()
+                                    }
+                    
+                    if let error = error {
+                        self?.showError(message: "Network Error: \(error.localizedDescription)")
+                        return
+                    }
+                    
+                    guard let httpResponse = response as? HTTPURLResponse else {
+                        self?.showError(message: "Invalid Response.")
+                        return
+                    }
+                    
+                    DispatchQueue.main.async {
+                        switch httpResponse.statusCode {
+                        case 200...299:
+                            self?.handleSuccessResponse(data: data)
+                        default:
+                            
+                            self?.showError(message: "Unknown Error")
+                        }
+                    }
+                }.resume()
+            } catch {
+                activityIndicator.stopAnimating()
+                showError(message: "Error creating the request.")
+            }
+        }
+        
+        private func handleSuccessResponse(data: Data?) {
+            DispatchQueue.main.async {
+                       self.activityIndicator.stopAnimating()
+                       self.tableViewBabies.reloadData()
+                   }
+            guard let data = data else {
+                showError(message: "Empty Response data.")
+                return
+            }
+            
+            do {
+                let babiesResponse = try JSONDecoder().decode(BabiesResponse.self, from: data)
+                babies = babiesResponse.data
+                DispatchQueue.main.async {
+                    self.tableViewBabies.reloadData()
+                }
+                
+                if self.babies.isEmpty {
+                    self.showNoBabiesMessage()
+                } else {
+                    self.hideNoBabiesMessage()
+                }
+            } catch {
+                if let jsonString = String(data: data, encoding: .utf8) {
+                        showError(message: "Error decoding JSON: \(error.localizedDescription)\nJSON: \(jsonString)")
+                    } else {
+                        showError(message: "Error decoding JSON: \(error.localizedDescription)")
+                    }
+            }
+        }
+        
     
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "sgBabiesDetail",
+               let destinationVC = segue.destination as? BabyDetailsViewController {
+                
+                if let selectedBaby = sender as? Baby {
+                    print(" Enviando baby: \(selectedBaby)")
+                    destinationVC.selectedBaby = selectedBaby
+                } else {
+                    print(" El sender no es un Baby o es nil")
+                    if let sender = sender {
+                        print("Tipo real del sender: \(type(of: sender))")
+                    } else {
+                        print("Sender es nil")
+                    }
+                }
+            }
+        }
     
-    
-    var babies: [baby] = [
-        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
-        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
-        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
-        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
-        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
-        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
-        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1),
-        baby(id: 1, name: "Manuel Medrano Castro", birthdate: Date() , incubator: 1)
-    ]
-    
-    
-    
-}
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            tableView.deselectRow(at: indexPath, animated: true)
+            
+            let selectedBaby = babies[indexPath.row]
+            print(" Baby seleccionado: \(selectedBaby)")
+            print("Tipo del objeto enviado: \(type(of: selectedBaby))") 
+            performSegue(withIdentifier: "sgBabiesDetail", sender: selectedBaby)
+        }
+       
+        private func showNoBabiesMessage() {
+            let messageLabel: UILabel = {
+                let label = UILabel()
+                label.text = "No babies available"
+                label.textColor = .gray
+                label.textAlignment = .center
+                label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+                label.numberOfLines = 0
+                return label
+            }()
+            
+            let containerView = UIView(frame: CGRect(x: 0, y: 0, width: tableViewBabies.bounds.width, height: 100))
+            messageLabel.frame = containerView.bounds
+            containerView.addSubview(messageLabel)
+            
+            tableViewBabies.backgroundView = containerView
+        }
+        
+        private func hideNoBabiesMessage() {
+            tableViewBabies.backgroundView = nil
+        }
+        
+        private func showError(message: String) {
+            DispatchQueue.main.async {
+                self.activityIndicator.stopAnimating()
+                let alert = UIAlertController(
+                    title: "Error",
+                    message: message,
+                    preferredStyle: .alert
+                )
+                alert.addAction(UIAlertAction(title: "OK", style: .default))
+                self.present(alert, animated: true)
+            }
+        }
+        
+        private let activityIndicator: UIActivityIndicatorView = {
+               let indicator = UIActivityIndicatorView(style: .large)
+               indicator.color = .gray
+               indicator.translatesAutoresizingMaskIntoConstraints = false
+               return indicator
+           }()
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return babies.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell  = tableViewBabies.dequeueReusableCell(withIdentifier: "BabyCell", for:indexPath)as! BabyTableViewCell
+            let baby = babies[indexPath.row]
+            cell.lblName.text = baby.full_name
+            cell.lblBirthDate.text = baby.date_of_birth
+            cell.lblIncubator.text = baby.incubator_id != nil ? String(baby.incubator_id!) : "sin asignar"
 
+            return cell
+        }
+    }
+
+    struct BabiesResponse:Decodable {
+        let data: [Baby]
+    }
+
+    struct Baby:Decodable{
+        let id: Int?;
+        let date_of_birth: String?;
+        let created_at: String?;
+        let full_name: String?;
+        let incubator_id:Int?;
+    }
--- NeoCareApp/Babies & relatives/BabyDetailsViewController.swift
+++ NeoCareApp/Babies & relatives/BabyDetailsViewController.swift
@@ -9,34 +9,199 @@ import UIKit
 
 class BabyDetailsViewController: UIViewController, UITableViewDataSource {
     
+    var selectedBaby: Baby?
+    var relatives: [Familiar] = []
+    let relativesEndPoint = "http://34.215.209.108/api/v1/relativesNoPaginate"
+    
+    @IBOutlet weak var lblBirthDate: UILabel!
+    @IBOutlet weak var lblNombre: UILabel!
+    
     @IBOutlet weak var viewAddIncubator: UIView!
     
     @IBOutlet weak var viewAddFamiliar: UIView!
     
     @IBOutlet weak var tableViewRelatives: UITableView!
     
+    @IBOutlet weak var viewBgInfo: UIView!
+    @IBOutlet weak var viewInfo: UIView!
     //inicializador
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+        
+        setupActivityIndicator()
         tableViewRelatives.dataSource = self
-        viewAddIncubator.roundCorners([.allCorners], 40.0)
-        viewAddFamiliar.roundCorners([.allCorners], 40.0)
+        
+        viewAddIncubator.roundCorners([.allCorners], 30.0)
+        viewAddFamiliar.roundCorners([.allCorners], 30.0)
+        viewInfo.roundCorners([.allCorners], 30.0)
+        viewBgInfo.roundCorners([.allCorners], 30.0)
+        
+        getRelatives()
+    }
+    
+    public func setup(){
+        if let baby = selectedBaby {
+            lblNombre.text = baby.full_name
+            lblBirthDate.text = baby.date_of_birth
+        } else{
+            print("selectedBaby es nil")
+        }
     }
     
     @IBAction func Regresar() {
         dismiss(animated: true)
     }
     
-    // table view
-    struct familiar{
-        let id: Int
-        let name:String
-        let phone:String
-        let email:String
+    private func setupActivityIndicator() {
+            view.addSubview(activityIndicator)
+            
+            NSLayoutConstraint.activate([
+                activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
+        }
     
+    private func createURLRequest() throws -> URLRequest {
+        guard let babyId = selectedBaby?.id else {
+                throw NetworkError.invalidURL
+            }
+            
+            let urlString = "\(relativesEndPoint)/\(babyId)"
+            guard let url = URL(string: urlString) else {
+                throw NetworkError.invalidURL
+            }
+            
+            let token = AuthManager.shared.loadToken() ?? ""
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("Bearer "+token, forHTTPHeaderField: "Authorization")
+            return request
     }
     
-
+    private func getRelatives() {
+        activityIndicator.startAnimating()
+        do {
+            let request = try createURLRequest()
+            
+            URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+                DispatchQueue.main.async {
+                    self?.activityIndicator.stopAnimating()
+                }
+                
+                if let error = error {
+                    self?.showError(message: "Error de red: \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    self?.showError(message: "Respuesta inválida del servidor")
+                    return
+                }
+                
+                // Depuración: Imprime el código de estado y los datos
+                print("Status Code:", httpResponse.statusCode)
+                if let data = data, let jsonString = String(data: data, encoding: .utf8) {
+                    print("Response Data:", jsonString)
+                }
+                
+                DispatchQueue.main.async {
+                    switch httpResponse.statusCode {
+                    case 200...299:
+                        self?.handleSuccessResponse(data: data)
+                    case 400:
+                        self?.showError(message: "Solicitud incorrecta")
+                    case 401:
+                        self?.showError(message: "No autorizado - token inválido")
+                    case 404:
+                        self?.showError(message: "No se encontraron familiares para este bebé")
+                    case 500:
+                        self?.showError(message: "Error interno del servidor")
+                    default:
+                        self?.showError(message: "Error desconocido - Código: \(httpResponse.statusCode)")
+                    }
+                }
+            }.resume()
+        } catch {
+            activityIndicator.stopAnimating()
+            showError(message: "Error creando la solicitud: \(error.localizedDescription)")
+        }
+    }
+    
+    
+    private func handleSuccessResponse(data: Data?) {
+        DispatchQueue.main.async {
+                   self.activityIndicator.stopAnimating()
+                    self.tableViewRelatives.reloadData()
+               }
+        guard let data = data else {
+            showError(message: "Empty Response data.")
+            return
+        }
+        
+        do {
+            let relativesResponse = try JSONDecoder().decode(RelativesResponse.self, from: data)
+            relatives = relativesResponse.data
+            DispatchQueue.main.async {
+                self.tableViewRelatives.reloadData()
+            }
+            
+            if self.relatives.isEmpty {
+                self.showNoRelativesMessage()
+            } else {
+                self.hideNoRelativesMessage()
+            }
+        } catch {
+            if let jsonString = String(data: data, encoding: .utf8) {
+                    showError(message: "Error decoding JSON: \(error.localizedDescription)\nJSON: \(jsonString)")
+                } else {
+                    showError(message: "Error decoding JSON: \(error.localizedDescription)")
+                }
+        }
+    }
+    
+    private func showNoRelativesMessage() {
+        let messageLabel: UILabel = {
+            let label = UILabel()
+            label.text = "No babies available"
+            label.textColor = .gray
+            label.textAlignment = .center
+            label.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+            label.numberOfLines = 0
+            return label
+        }()
+        
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: tableViewRelatives.bounds.width, height: 100))
+        messageLabel.frame = containerView.bounds
+        containerView.addSubview(messageLabel)
+        
+        tableViewRelatives.backgroundView = containerView
+    }
+    
+    private func hideNoRelativesMessage() {
+        tableViewRelatives.backgroundView = nil
+    }
+    
+    private func showError(message: String) {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            let alert = UIAlertController(
+                title: "Error",
+                message: message,
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    private let activityIndicator: UIActivityIndicatorView = {
+           let indicator = UIActivityIndicatorView(style: .large)
+           indicator.color = .gray
+           indicator.translatesAutoresizingMaskIntoConstraints = false
+           return indicator
+       }()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return relatives.count
@@ -45,33 +210,23 @@ class BabyDetailsViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FamiliarCell", for: indexPath)as! FamiliarTableViewCell
         let familiar = relatives[indexPath.row]
-        cell.lblName.text = familiar.name
-        cell.lblPhone.text = familiar.phone
+        cell.lblName.text = familiar.full_name
+        cell.lblPhone.text = familiar.phone_number
         cell.lblEmail.text = familiar.email
         return cell
     }
     
-    var relatives: [familiar] = [
-        familiar(id: 1, name: "jona", phone: "8715066383", email: "jona@gmail.com"),
-        familiar(id: 1, name: "jona", phone: "8715066383", email: "jona@gmail.com"),
-        familiar(id: 1, name: "jona", phone: "8715066383", email: "jona@gmail.com"),
-        familiar(id: 1, name: "jona", phone: "8715066383", email: "jona@gmail.com"),
-        familiar(id: 1, name: "jona", phone: "8715066383", email: "jona@gmail.com"),
-        familiar(id: 1, name: "jona", phone: "8715066383", email: "jona@gmail.com"),
-        familiar(id: 1, name: "jona", phone: "8715066383", email: "jona@gmail.com"),
-    ]
     
 }
-    
 
-    /*
-    // MARK: - Navigation
+struct RelativesResponse:Decodable {
+    let data: [Familiar]
+}
+struct Familiar:Decodable{
+    let full_name: String
+    let phone_number:String
+    let email:String
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+}
 
 
